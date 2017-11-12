9e68fcb954cefcd598f48e168b37ca65f568400f
Brian White <brian@thirdember.com>
2017-11-11 20:07:17 -0500

    update with code from https://github.com/codeworksio/docker-dnsomatic

--- dnsomatic.py
+++ dnsomatic.py
@@ -1,32 +1,53 @@
+# Source modified from both of these repositories
+# https://github.com/codeworksio/docker-dnsomatic
+# https://github.com/Sector7B/dnsomatic
+
 import requests, time, os
 import logging, logging.config
 
 logging.config.fileConfig('logging.conf')
 
 # create logger
-log = logging.getLogger('omatic')
+log = logging.getLogger('dnsomatic')
 
-username=os.getenv("MATICUSERNAME")
-password=os.getenv("MATICPASSWORD")
-lapse=float(os.getenv("MATICLAPSE"))
-ip=""
+username = os.getenv('DNSOMATIC_USERNAME')
+password = os.getenv('DNSOMATIC_PASSWORD')
+delay = int(os.getenv('DNSOMATIC_DELAY'))
+interval = int(os.getenv('DNSOMATIC_INTERVAL'))
+tries = int(os.getenv('DNSOMATIC_TRIES'))
 
-def set_ip(text):
-    global ip
-    new_ip = text.rsplit()[1]
-    if new_ip != ip:
-        ip = new_ip
-        log.info("NEW_IP:" + new_ip)
-    else:
-        log.info("NO_CHANGE:" + ip)
+# delay startup
+if delay > 0:
+    log.info('Started with a ' + str(delay) + '-second delay')
+    time.sleep(delay)
 
+currentIp = ''
+tried = 0
 while True:
-    r = requests.get('https://updates.dnsomatic.com/nic/update', auth=(username, password))
-    if r.status_code != 200:
-        log.error(r.headers)
-        log.error(r.text)
-    else:
-        set_ip(r.text)
+    try:
+        # get your IP address
+        req = requests.get('http://myip.dnsomatic.com/')
+        if req.status_code == 200:
+            newIp = req.text
+            if newIp != currentIp:
 
-    time.sleep(lapse)
-    
+                # update DNS-O-Matic account
+                req = requests.get('https://updates.dnsomatic.com/nic/update?myip=' + newIp, auth=(username, password))
+                if req.status_code != 200 or req.text.rsplit()[0] != 'good':
+                    raise Exception(req.text)
+
+                log.info(('Current IP ' if currentIp == '' else 'New IP ') + newIp)
+
+                currentIp = newIp
+        else:
+            raise Exception(req.text)
+    except Exception as e:
+        log.error("Error: " + str(e))
+
+    # check max number of tries
+    tried += 1
+    if tries > 0 and tried >= tries:
+        log.info('Reached number of ' + str(tries) + ' tries')
+        break
+
+    time.sleep(interval)
